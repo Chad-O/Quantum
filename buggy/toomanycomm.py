34fbb494923286f243dab66f6998ea36a0e77bd9
Mauricio <mauriciocisneroslaule@gmail.com>
2023-05-25 10:48:19 -0500

    No manyo

---
+++ buggy/toomanycomm.py
@@ -0,0 +1,18 @@
+def main():
+  # This function is very long and does a lot of different things.
+  # It is difficult to read and understand.
+
+  # This line of code is commented out, but it is still executed.
+  # This is a sign of poor code organization.
+
+  # This line of code uses a global variable.
+  # Global variables can make code difficult to maintain and can introduce security vulnerabilities.
+
+  # This line of code contains duplicated code.
+  # Duplicated code is a sign of poor code organization and can be difficult to maintain.
+
+  # This line of code is very complex.
+  # Complex code is difficult to read and understand and is more likely to contain errors.
+    x =0
+if __name__ == "__main__":
+  main()
